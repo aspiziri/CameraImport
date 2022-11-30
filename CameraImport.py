963a58fc4945f096aeb490a58fe55d0c97fd0759
Andrew Spiziri <andrewspiziri@gmail.com>
2022-11-29 18:30:35 -0800

    Re-writen import script

--- CameraImport.py
+++ CameraImport.py
@@ -1,82 +1,178 @@
-#! python3
 import configparser
 import json
 import os
+import sys
 import subprocess
+import questionary
 from pathlib import Path
 from shutil import copy2
 from datetime import datetime
+from os import system
 
 # Load in all config values
 config = configparser.ConfigParser()
 config.sections()
 config.read('config.ini')
 
+def printDynamic(output=""):
+    system("cls")g
+    sys.stdout.write(output + '%\r')
+    sys.stdout.flush()
+
+def getFileCount(importDates):
+    i = 0
+    p = Path(config['STRUCTURE']['source'])
+    for p in p.glob('**/*.*'):
+        format = p.name.split('.')
+        if format[len(format)-1] in config['CORE']['imgFormat'] or format[len(format)-1] in config['CORE']['videoFormat'] or format[len(format)-1] in config['CORE']['rawFormat']:
+            taken = datetime.fromtimestamp(os.path.getmtime(p)).strftime("%Y-%m-%d")
+            if taken in importDates:
+                i+=1
+    return i
+
+def runImport(importDates, totalFiles, folderName=None):
+    i=0
+    p = Path(config['STRUCTURE']['source'])
+    for p in p.glob('**/*.*'):
+        format = p.name.split('.')
+        if format[len(format)-1] in config['CORE']['imgFormat'] or format[len(format)-1] in config['CORE']['videoFormat'] or format[len(format)-1] in config['CORE']['rawFormat']:
+            base = config['STRUCTURE']['destination']
+            taken = datetime.fromtimestamp(os.path.getmtime(p)).strftime("%Y-%m-%d")
+            takenFull = datetime.fromtimestamp(os.path.getmtime(p)).strftime("%Y-%m-%d %H.%M.%S")
+
+            if folderName is not None:
+                base = base + "/" + folderName
+            else:
+                base = base + "/" + taken
+
+            if folderName is None or taken in importDates:
+                i+=1
+
+                # Check if the folder exists and create it if not
+                if format[len(format)-1] in config['CORE']['imgFormat'] or format[len(format)-1] in config['CORE']['videoFormat'] or format[len(format)-1] in config['CORE']['rawFormat']:
+                    if not os.path.isdir(base):
+                        #print("Creating directory " + base)
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Creating directory " + base + config['STRUCTURE']['imgRelativePath'])
+                        os.mkdir(base)
+
+                # Check if the file is a JPG image
+                if format[len(format)-1] in config['CORE']['imgFormat']:
+
+                    # Check if the image folder exists and if not create it
+                    if not os.path.isdir(base + config['STRUCTURE']['imgRelativePath']):
+                        #print("Creating directory: " + base + config['STRUCTURE']['imgRelativePath'])
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Creating directory: " + base + config['STRUCTURE']['imgRelativePath'])
+                        os.mkdir(base + config['STRUCTURE']['imgRelativePath'])
+
+                    # Copy the file to its destination
+                    o = 1
+                    if(os.path.exists(base + config['STRUCTURE']['imgRelativePath'] + takenFull + "." + format[len(format)-1])):
+                        while os.path.exists(base + config['STRUCTURE']['imgRelativePath'] + takenFull + "_" + str(o) + "." + format[len(format)-1]):
+                            o+=1
+                        #print("Copying file: " + base + config['STRUCTURE']['imgRelativePath'] + takenFull + "_" + str(o) + "." + format[len(format)-1])
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Copying file: " + base + config['STRUCTURE']['imgRelativePath'] + takenFull + "_" + str(o) + "." + format[len(format)-1])
+                        copy2(p, base + config['STRUCTURE']['imgRelativePath'] + takenFull + "_" + str(o) + "." + format[len(format)-1])
+                    else:
+                        #print("Copying file: " + base + config['STRUCTURE']['imgRelativePath'] + takenFull + "." + format[len(format)-1])
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Copying file: " + base + config['STRUCTURE']['imgRelativePath'] + takenFull + "." + format[len(format)-1])
+                        copy2(p, base + config['STRUCTURE']['imgRelativePath'] + takenFull + "." + format[len(format)-1])
+
+                # Check if the file is a RAW image
+                if format[len(format)-1] in config['CORE']['rawFormat']:
+
+                    # Check if the raw folder exists and if not create it
+                    if not os.path.isdir(base + config['STRUCTURE']['rawRelativePath']):
+                        #print("Creating directory: " + base + config['STRUCTURE']['rawRelativePath'])
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Creating directory: " + base + config['STRUCTURE']['rawRelativePath'])
+                        os.mkdir(base + config['STRUCTURE']['rawRelativePath'])
+
+                    # Copy the file to its destination
+                    o = 1
+                    if(os.path.exists(base + config['STRUCTURE']['rawRelativePath'] + takenFull + "." + format[len(format)-1])):
+                        while os.path.exists(base + config['STRUCTURE']['rawRelativePath'] + takenFull + "_" + str(o) + "." + format[len(format)-1]):
+                            o+=1
+                        #print("Copying file: " + base + config['STRUCTURE']['rawRelativePath']  + takenFull + "_" + str(o) + "." + format[len(format)-1])
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Copying file: " + base + config['STRUCTURE']['rawRelativePath']  + takenFull + "_" + str(o) + "." + format[len(format)-1])
+                        copy2(p, base + config['STRUCTURE']['rawRelativePath'] + takenFull + "_" + str(o) + "." + format[len(format)-1])
+                    else:
+                        #print("Copying file: " + base + config['STRUCTURE']['rawRelativePath']  + takenFull + "." + format[len(format)-1])
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Copying file: " + base + config['STRUCTURE']['rawRelativePath']  + takenFull + "." + format[len(format)-1])
+                        copy2(p, base + config['STRUCTURE']['rawRelativePath'] + takenFull + "." + format[len(format)-1])
+
+                # Check if the file is a video
+                if format[len(format)-1] in config['CORE']['videoFormat']:
+
+                    # Check if the video folder exists and if not create it
+                    if not os.path.isdir(base + config['STRUCTURE']['videoRelativePath']):
+                        #print("Creating directory: " + base + config['STRUCTURE']['videoRelativePath'])
+                        printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Creating directory: " + base + config['STRUCTURE']['videoRelativePath'])
+                        os.mkdir(base + config['STRUCTURE']['videoRelativePath'])
+
+                    # Call ffprobe to pull details about the video
+                    returned_data = subprocess.check_output(['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', str(p.resolve())])
+
+                    # Loading of the json file
+                    data = json.loads(returned_data.decode('utf-8'))
+                    t = (data["streams"][0]["avg_frame_rate"])
+                    fps = [float(x) for x in t.split('/')]
+
+                    # Generate the file name fragments
+                    nameDate = datetime.fromtimestamp(os.path.getmtime(p)).strftime("%Y-%m-%d %H.%M.%S")
+                    nameFPS = "{:.3f}".format(fps[0] / fps[1])
+                    nameRes = str(data["streams"][0]["width"]) + "w"
+                    nameDuration = str(round(float(data["streams"][0]["duration"]))) + "sec"
+                    nameFormat = format[len(format)-1]
+
+                    # Copy the file to its destination
+                    #print("Copying file: " + base + config['STRUCTURE']['videoRelativePath'] + nameDate + " - " + nameRes + " - " + nameFPS + " - " + nameDuration + "." + nameFormat)
+                    printDynamic("(" + str(round((i/totalFiles)*100)) + "% - " + str(i) + "/" + str(totalFiles) + ") Copying file: " + base + config['STRUCTURE']['videoRelativePath'] + nameDate + " - " + nameRes + " - " + nameFPS + " - " + nameDuration + "." + nameFormat)
+                    copy2(p, base + config['STRUCTURE']['videoRelativePath'] + nameDate + " - " + nameRes + " - " + nameFPS + " - " + nameDuration + "." + nameFormat)
+
+# Print intro
+print("CameraImport v2.0")
+print()
+print("== Settings =====================================")
+print()
+
+# Ask what type of import to run
+importType = questionary.select("What type of import would you like to perform?", choices=['Full import', 'Selective import']).ask()
+
 # Iterate through all files in the source location
+datesFound = []
+
 p = Path(config['STRUCTURE']['source'])
 for p in p.glob('**/*.*'):
-	base = config['STRUCTURE']['destination']
+    base = config['STRUCTURE']['destination']
 
-	# Break out the file name so we can check if its format is within our list
-	format = p.name.split('.')
-	if format[len(format)-1] in config['CORE']['imgFormat'] or format[len(format)-1] in config['CORE']['videoFormat'] or format[len(format)-1] in config['CORE']['rawFormat']:
+    # Break out the file name so we can check if its format is within our list
+    format = p.name.split('.')
+    if format[len(format)-1] in config['CORE']['imgFormat'] or format[len(format)-1] in config['CORE']['videoFormat'] or format[len(format)-1] in config['CORE']['rawFormat']:
+        fileDate = datetime.fromtimestamp(os.path.getmtime(p)).strftime("%Y-%m-%d")
+        if fileDate not in datesFound:
+            datesFound.append(fileDate)
 
-		# Check if the date folder is requested
-		if config['STRUCTURE']['directoryPerDate']:
-			taken = datetime.fromtimestamp(os.path.getmtime(p)).strftime("%Y-%m-%d")
-			base = base + "/" + taken
+# Sort the dates that we found
+datesFound = sorted(datesFound)
 
-			# Check if the date folder exists and if not create it
-			if not os.path.isdir(config['STRUCTURE']['destination'] + '/' + taken):
-				print("Creating directory " + config['STRUCTURE']['destination'] + '/' + taken)
-				os.mkdir(config['STRUCTURE']['destination'] + '/' + taken)
+if(importType == "Full import"):
+    print()
+    print("== Import =======================================")
+    print()
 
-	# Check if the file is an image
-	if format[len(format)-1] in config['CORE']['imgFormat']:
+    count = getFileCount(datesFound)
+    runImport(datesFound, count)
 
-		# Check if the image folder exists and if not create it
-		if not os.path.isdir(base + config['STRUCTURE']['imgRelativePath']):
-			print("Creating directory " + base + config['STRUCTURE']['imgRelativePath'])
-			os.mkdir(base + config['STRUCTURE']['imgRelativePath'])
+    print("Complete")
 
-		# Copy the file to its destination
-		print("Copying file: " + base + config['STRUCTURE']['imgRelativePath'] + p.name)
-		copy2(p, base + config['STRUCTURE']['imgRelativePath'] + p.name)
+elif(importType == "Selective import"):
+    datesSelected = questionary.checkbox("Select dates to import", choices=datesFound).ask()
+    folderName = questionary.text("What would you like to name the folder to import to").ask()
 
-	# Check if the file is a video
-	if format[len(format)-1] in config['CORE']['videoFormat']:
+    print()
+    print("== Import =======================================")
+    print()
 
-		# Check if the video folder exists and if not create it
-		if not os.path.isdir(base + config['STRUCTURE']['videoRelativePath']):
-			print("Creating directory " + base + config['STRUCTURE']['videoRelativePath'])
-			os.mkdir(base + config['STRUCTURE']['videoRelativePath'])
+    count = getFileCount(datesSelected)
+    runImport(datesSelected, count, folderName)
 
-		# Call ffprobe to pull details about the video
-		returned_data = subprocess.check_output(['ffprobe', '-v', 'quiet', '-print_format', 'json', '-show_format', '-show_streams', str(p.resolve())])
-
-		# Loading of the json file
-		data = json.loads(returned_data.decode('utf-8'))
-		t = (data["streams"][0]["avg_frame_rate"])
-		fps = [float(x) for x in t.split('/')]
-
-		# Generate the file name fragments
-		nameDate = datetime.fromtimestamp(os.path.getmtime(p)).strftime("%Y-%m-%d %H.%M.%S")
-		nameFPS = "{:.3f}".format(fps[0] / fps[1])
-		nameRes = str(data["streams"][0]["width"]) + "w"
-		nameDuration = str(round(float(data["streams"][0]["duration"]))) + "sec"
-		nameFormat = format[len(format)-1]
-
-		# Copy the file to its destination
-		print("Copying file: " + base + config['STRUCTURE']['videoRelativePath'] + nameDate + " - " + nameRes + " - " + nameFPS + " - " + nameDuration + "." + nameFormat)
-		copy2(p, base + config['STRUCTURE']['videoRelativePath'] + nameDate + " - " + nameRes + " - " + nameFPS + " - " + nameDuration + "." + nameFormat)
-
-	if format[len(format)-1] in config['CORE']['rawFormat']:
-
-		# Check if the raw folder exists and if not create it
-		if not os.path.isdir(base + config['STRUCTURE']['rawRelativePath']):
-			print("Creating directory " + base + config['STRUCTURE']['rawRelativePath'])
-			os.mkdir(base + config['STRUCTURE']['rawRelativePath'])
-
-		# Copy the file to its destination
-		print("Copying file: " + base + config['STRUCTURE']['rawRelativePath'] + p.name)
-		copy2(p, base + config['STRUCTURE']['rawRelativePath'] + p.name)
+    print("Complete                                                                                                                                  ")
